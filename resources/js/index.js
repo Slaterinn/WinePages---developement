e2ffbbde195dc0440e52743850aa7a9f122b9898
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2023-06-23 23:50:27 +0000

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -216,7 +216,7 @@ filterData = (data, query) => {
     }
 
     for (let key in query) {
-        console.log(item['food_pairing'] + ' - ' + query['food_pairing'])
+        console.log(item['food_pairing'] + ' - ' + query['food_pairing'] + query['food_pairing'].includes(item['food_pairing']) )
         if (item[key] === undefined || item[key] === null || query[key] !==(item[key]).toUpperCase() || parseInt(item['price']) > filter['price'] || !query['food_pairing'].includes(item['food_pairing']) ) {
           return false;
         }
