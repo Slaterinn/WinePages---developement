87db2452f90b9f74e7de4e47f970b3866e554034
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2023-11-10 13:52:04 +0000

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -281,7 +281,10 @@ function appendData(data) {
         break;
       case "Sparkling Wine":
         wine_type_sel = "Freyðivín";
-
+        break;
+      default:
+        wine_type_sel = "N/A";
+        break;
     }
 
     var wine_type = document.createElement("h5");
